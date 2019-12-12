e9ed8b2e2bf7878aa6e2091c3857d8bd5d5ce168
Tysane Mlkshk <tmlkshk@at-q4.21-school.ru>
2019-12-12 15:26:49 +0300

    get_sharp.c

---
+++ get_sharp.c
@@ -0,0 +1,84 @@
+/************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_sharp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmlkshk <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/12 14:23:22 by tmlkshk           #+#    #+#             */
+/*   Updated: 2019/12/12 14:26:42 by tmlkshk          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+int	getmin(int a, int b, int c, int d)
+{
+	int min;
+
+	min = a;
+	if (min > b)
+		min = b;
+	if (min > c)
+		min = c;
+	if (min > d)
+		min = d;
+	return (min);
+}
+
+t_point	*optimize_sharps(t_point *sharps)
+{
+	t_point	*temp;
+	int 	count;
+	int		min;
+
+	temp = sharps;
+	min = getmin(temp[0].x, temp[1].x, temp[2].x, temp[3].x);
+	count = 0;
+	while (count < 4)
+	{
+		temp[count].x = temp[count].x - min;
+		count++;
+	}
+	min = getmin(temp[0].y, temp[1].y, temp[2].y, temp[3].y);
+	count = 0;
+	while (count < 4)
+	{
+		temp[count].y = temp[count].y - min;
+		count++;
+	}
+	return (temp);
+}
+
+t_point	*get_sharps(char *data)
+{
+	t_point	*sharps;
+	int 	sharp;
+	int 	i;
+
+	sharp = 0;
+	i = 0;
+	sharps = (t_point*)malloc(sizeof(t_point) * 4);
+	while (i < 20)
+	{
+		if (data[i] == '#')
+		{
+			sharps[sharp].x = i % 5;
+			sharps[sharp].y = i / 5;
+			sharp++;
+		}
+		i++;
+	}
+	sharps = optimize_sharps(sharps);
+	return (sharps);
+}
+
+int		main()
+{
+	char	*a;
+	t_point	*f;
+
+	a = "..#.\n..#.\n.#..\n.#..\n";
+	f = get_sharps(a);
+	return (0);
+}
